a0e732afebe843700fdbcee86fe524b7609f6958
trkhmrt <trkhamarat@gmail.com>
2024-02-05 21:06:46 +0300

    .

--- 4.HAFTA/05.02.24.py
+++ 4.HAFTA/05.02.24.py
@@ -24,9 +24,41 @@ for i in range(çalışma_aralığı):
 #     print(f"MERHABA:{indeks_numarasi}")
 
 
+
 # for indeks_numarasi in range(0,10,2):
 #     print(f"MERHABA:{indeks_numarasi}")
 
+#for indeks_numarasi in range(0,10,2):
+#     print(f"MERHABA:{indeks_numarasi}")
 
 
+#for i in range(10,0,-1):
+    #print(i)
+
+#For döngüsü ile 0-100 arasındaki çift sayıları bulun.
+
+# for i in range(0,100):
+#     if i%2==0:
+#         print(i,"Çift sayidir.")
+
+
+#for döngüsü ile 0 dan 10 a kadar olan sayıların karesini alınız.
+# for i in range(0,10):
+#     #print(i*i)
+#     #print(pow(i,2))
+#     print(i**2)
+
+
+#Başlangıç ve bitiş değerlerini kullanıcıdan alınan bir for döngüsü yazınız.Eğer başlşangıç bitişten büyükse döngüyü bir şekilde çalıştırın.
+
+baslangic=int(input("Başlangıç Değeri giriniz."))
+bitis=int(input("Bitiş değeri giriniz."))
+
+if baslangic>bitis:
+    for i in range(baslangic,bitis,-1):
+        print(i)
+else:
+    for i in range(baslangic,bitis):
+        print(i)
+       
 
